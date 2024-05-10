37ff69ba848c8bde8d160c3cc2c773726b4f23f4
OpenCo <admin@openco.ca>
2024-05-10 13:34:11 +0000

    Update download_model.sh to download 5 stems model

--- download_model.sh
+++ download_model.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p pretrained_models
-mkdir -p pretrained_models/2stems
-curl -o pretrained_models/2stems.tar.gz -L https://github.com/deezer/spleeter/releases/download/v1.4.0/2stems.tar.gz
-tar -xvzf pretrained_models/2stems.tar.gz -C pretrained_models/2stems
+mkdir -p pretrained_models/5stems
+curl -o pretrained_models/5stems.tar.gz -L https://github.com/deezer/spleeter/releases/download/v1.4.0/5stems-finetune.tar.gz
+tar -xvzf pretrained_models/5stems.tar.gz -C pretrained_models/5stems
